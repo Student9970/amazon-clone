6af701b021d1c0f99671eafd18fa3fd155cec1c9
Usama Ansari <student9970@gmail.com>
2022-01-22 18:27:44 +0530

    Just initialised the repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Guys, Let's build Amazon Store</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAAGU64Tkq4DZPXg90xrZt_uwvuzV6DjhQ",
+    authDomain: "clone-37bef.firebaseapp.com",
+    projectId: "clone-37bef",
+    storageBucket: "clone-37bef.appspot.com",
+    messagingSenderId: "445370920174",
+    appId: "1:445370920174:web:23dc483e4f26ddef7797ff",
+    measurementId: "G-PB8FZ1B4EN"
+};
